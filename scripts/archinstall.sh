c3448d7a39e4ff10e6b5a6002386b37eec18d569
Tom Keene <tom@theanthillsocial.co.uk>
2014-05-02 16:44:36 +0000

    Added T440p specific code

--- scripts/archinstall.sh
+++ scripts/archinstall.sh
@@ -1,29 +1,67 @@
 # SET VARIABLES
 HOSTNAME=ArchT440
 DEFAULTUSER=tom
+REALNAME="Tom Keene"
+WIFIINTERFACE=wlp4so
+ETHINTERFACE=enp0s25
 
 start_me(){
 	#installA
+	#installnetwork
+	#installmanual
 	#installB
-	installC
+	#installC
+	#installvideo
+	#installT440p
+	#installdesktop
+	installcinnamon
+	#checkforerrors
+}
+installcinnamon(){
+	pacman -S cinnamon
+	echo "Add to: pacman.conf\n[cinnamon]\nServer = http://www.equinox-project.org/repos/arch/$arch\n"
+	echo "Add to: ~/.xinitrc\nexec cinnamon-session"
+}
+checkforerrors(){
+ 	# search for potential errors in dmesg or journalctl
+	dmesg | egrep -i -A2 -B2 'unsupported|warn|error|disabl|not|doesn|bug' 
+}
+instalvideo(){
+	# Video
+	pacman -S xorg-server xorg-xinit xorg-server-utils mesa xorg-twm xorg-xclock xterm 
+
+	# Intel driver
+	pacman -S xf86-video-intel lib32-intel-dri
+	
+	# for newer Intel GPU's
+	pacman -S libva-intel-driver
+}
+installT440p(){
+	#video dependencies
+	pacman -S intel-dri xf86-video-intel bumblebee primus virtualgl 
+        
+	#Potential issues with:
+	#pacman -S bbswitch
+	#pacman -S lib32-virtualgl lib32-primus nvidia lib32-nvidia-utils lib32-intel-dri
+	
+	# Video switching
+	gpasswd -a root bumblebee
+	gpasswd -a $DEFAULTUSER bumblebee
+	systemctl enable bumblebeed.service
+	systemctl start bumblebeed.service
 }
 installC(){
-	# Install packer
+	# TODO: This section needs work!!
+	# Install packer 
 	pacman -S base-devel fakeroot jshon expac
 	mkdir /opt/packer
+        chown $DEFAULTUSER /opt/packer
         cd /opt/packer
   	wget https://aur.archlinux.org/packages/pa/packer/PKGBUILD
         makepkg
         pacman -U packer-*.pkg.tar.gz
 }
 installB(){
-	pacman -S ntp
-	cp /etc/ntp.conf /etc/ntp.conf.backup
-	echo -e "server 0.pool.ntp.org iburst\nserver 1.pool.ntp.org iburst\nserver 2.pool.ntp.org iburst\nserver 3.pool.ntp.org iburst\n" > /etc/ntp.conf
-
-	# Video
-	pacman -S xorg-server xorg-xinit xorg-server-utils mesa xorg-twm xorg-xclock xterm
-
         #Mouse and trackpad
         pacman -S xf86-input-synaptics
 	
@@ -36,7 +74,7 @@ installA(){
 	# set the root password and default user
 	echo -e "\nSET ROOT PASSWORD:\n"
 	passwd
-	useradd -m -g -G wheel,storage,power -s /bin/bash $DEFAULTUSER
+	useradd -m -g $DEFAULTUSER -G wheel,storage,power,network -c $REALNAME -s /bin/bash $DEFAULTUSER
 	echo -e "\nSET USER PASSWORD FOR $DEFAULTUSER:\n"
 	passwd $DEFAULTUSER
 
@@ -63,22 +101,31 @@ installA(){
 	setfont ter-p16n.psf.gz
 	echo -e "KEYMAP=uk\nFONT=ter-p16n.psf.gz" > /etc/vconsole.conf
 
-	# Set time zone
+	# Set time 
 	ln -s /usr/share/zoneinfo/Greenwich /etc/localtime
 	hwclock --systohc --utc
 	systemctl start ntpd.service
 	systemctl enable ntpd.service
 	ntpq -np
+	pacman -S ntp
+	cp /etc/ntp.conf /etc/ntp.conf.backup
+	echo -e "server 0.pool.ntp.org iburst\nserver 1.pool.ntp.org iburst\nserver 2.pool.ntp.org iburst\nserver 3.pool.ntp.org iburst\n" > /etc/ntp.conf
 
 	# Set a hostname
 	cp /etc/hosts /etc/hosts.backup
 	echo $HOSTNAME > /etc/hostname
 	echo "127.0.0.1	localhost.localdomain	localhost	$HOSTNAME" >> /etc/hosts
-
-	# Setup network for: wiredAutoplug, wifimenu, netctl-auto
+}
+installnetwork(){
+	# Setup network for: wiredAutoplug, wifimenu, netctl-auto rfkill
 	pacman -S ifplugd dialog wpa_actiond
+	ip link set $WIFIINTERFACE up
+	ip link set $ETHINTERFACE up
 	ip a
-
+        systemctl enable netctl-ifplugd@ETHITERFACE.service
+        systemctl enable netctl-auto@WIFIINTERFACE.service
+}
+installmanual(){
 	# Do by hand
 	echo -e "DO THESE FINAL THINGS BY HAND:\n"
 	echo "RUN:\nEDITOR=vim visudo"

fd910e3ad03a6ef8ed1526394408722374927c53
root <root@localhost.localdomain>
2014-05-13 09:47:47 +0000

    Added auto interface discover

--- scripts/networkcreate_ap.sh
+++ scripts/networkcreate_ap.sh
@@ -1,12 +1,13 @@
-WIFI=wlan0
-ETH="enp3s0"
+WIFI=`ip link | grep "3: w*" -m 1| gawk -F: '{print $2}'`
+ETH=`ip link | grep "2: e*" -m 1 | gawk -F: '{print $2}'`
 SSID="goat"
 PASS="123456768"
 NO_VIRT="--no-virt"
+echo -e "\n\nWIFI=$WIFI ETH=$ETH SSID=$SSID PASS=$PASS NO_VIRT=$NO_VIRT\n\n"
 
 
 ### passphrase (open network):
-    #create_ap $WIFI $ETH $SSID $NO_VIRT
+    create_ap $WIFI $ETH $SSID $NO_VIRT
 
 ### WPA + WPA2 passphrase:
     #create_ap $WIFI $ETH $SSID $PASS $NO_VIRT
@@ -15,7 +16,7 @@ NO_VIRT="--no-virt"
     #create_ap -n $WIFI $SSID $PASS $NO_VIRT
 
 ### Bridged Internet sharing:
-    create_ap -m bridge $WIFI $ETH $SSID $PASS $NO_VIRT
+    #create_ap -m bridge $WIFI $ETH $SSID $PASS $NO_VIRT
 
 ### Internet sharing from the same WiFi interface:
     #create_ap $WIFI $WIFI $SSID $PASS $NO_VIRT

99e3b9a04f205a7464afabafe94fbad5b60b6b62
Tom Keene <tom@theanthillsocial.co.uk>
2014-06-03 08:41:04 +0100

    Added radio script

---
+++ scripts/btrfsdiskusage.sh
@@ -0,0 +1,12 @@
+echo -e "\n============= sudo btrfs filesystem show  ============="
+sudo btrfs filesystem show 
+
+echo -e "\n============= sudo btrfs filesystem df / ============="
+sudo btrfs filesystem df /
+
+echo -e "\n============= sudo btrfs filesystem df /home ============="
+sudo btrfs filesystem df /home
+
+echo -e "\n============= sudo btrfs filesystem df /mnt/media ============="
+sudo btrfs filesystem df /mnt/media
+

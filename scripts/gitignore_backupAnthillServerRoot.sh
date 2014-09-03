a26cd398fa4fea753d95fd512a6afc097f45ceed
Tom Keene <tom@theanthillsocial.co.uk>
2014-09-03 17:04:29 +0100

    Tidy

---
+++ scripts/gitignore_backupAnthillServerRoot.sh
@@ -0,0 +1,2 @@
+rsync -avzh -e ssh root@theanthillsocial.co.uk:/root /home/tom/backups/anthillsocialserverROOT
+rsync -avzh -e ssh root@theanthillsocial.co.uk:/var/www /home/tom/backups/anthillsocialserverWWW

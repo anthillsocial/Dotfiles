492dc6d03033e42537c8733a48143ccb6b94cd82
Tom Keene <tom@theanthillsocial.co.uk>
2014-09-03 17:06:08 +0100

    Delete gitignore_backupToNas.sh

--- scripts/gitignore_backupToNas.sh
+++
@@ -1,2 +0,0 @@
-echo "Logging on to ReadyNAS...";
-rsync -avzh /home/tomkeene -e ssh --delete --exclude-from '/home/tomkeene/Dropbox/Shellscripts/exludeRsyncFolders.txt' root@192.168.1.70:/c/backup/latest/archant
